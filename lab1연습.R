32099907ae0468d1b264e8d15243057425e7ca8c
Yoonji Kim <93129727+YJK0113@users.noreply.github.com>
2021-12-31 00:34:54 +0900

    lab1

---
+++ lab1연습.R
@@ -0,0 +1,23 @@
+123 + 456
+print("Hello world!")
+(0/2 + 53 + 72) %% 3
+x <- 3
+print(x*1.2345)
+seq(1,10)
+sum(1,10)
+sum(1:10)
+
+x<-6
+
+#scalars practical question
+anna_average <- 824/5
+bea_average <- 1042/8
+
+print(anna_average)
+print(bea_average)
+
+#logical vectors practical question
+mult3 <- seq(from=3,to=30,by=3)
+print(mult3 %% 2 == 0)
+
+print(sum(mult3%%2))
